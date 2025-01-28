ff204dba2e9578fbfbca005f93a79ee92523b921
sushantkhilare25 <sskhilare25@gmail.com>
2025-01-28 11:57:57 +0530

    dcdcdc

--- SpringBoot_2/src/main/java/com/cjc/main/crud/SpringBoot2Application.java
+++ SpringBoot_2/src/main/java/com/cjc/main/crud/SpringBoot2Application.java
@@ -10,7 +10,7 @@ public class SpringBoot2Application {
 		System.out.println("Sushant khilare");
 		System.out.println("Hemant sarkge");
 		System.out.println("Chetan patil");
-		System.out.println("Chetan patil");
+	
 		
 		SpringApplication.run(SpringBoot2Application.class, args);
 	}
