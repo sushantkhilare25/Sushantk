268d96c3381ec72a075651fea69559f7deacbb0a
sushantkhilare25 <sskhilare25@gmail.com>
2025-01-28 11:40:06 +0530

    Sushant new project

---
+++ SpringBoot_2/src/main/java/com/cjc/main/crud/SpringBoot2Application.java
@@ -0,0 +1,14 @@
+package com.cjc.main.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2Application {
+
+	public static void main(String[] args) {
+		System.out.println("Sushant khilare");
+		SpringApplication.run(SpringBoot2Application.class, args);
+	}
+
+}
