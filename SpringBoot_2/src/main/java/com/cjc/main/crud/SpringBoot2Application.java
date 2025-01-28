24c6e7ce652888d2272ce0b6b02bd2704bfd0e17
sushantkhilare25 <sskhilare25@gmail.com>
2025-01-28 11:51:46 +0530

    Add printing Statement by sushant

--- SpringBoot_2/src/main/java/com/cjc/main/crud/SpringBoot2Application.java
+++ SpringBoot_2/src/main/java/com/cjc/main/crud/SpringBoot2Application.java
@@ -9,6 +9,7 @@ public class SpringBoot2Application {
 	public static void main(String[] args) {
 		System.out.println("Sushant khilare");
 		System.out.println("Hemant sarkge");
+		System.out.println("Chetan patil");
 		SpringApplication.run(SpringBoot2Application.class, args);
 	}
 
